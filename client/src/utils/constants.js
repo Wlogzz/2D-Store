633a90603aa9db8e494409099a552b4484e4590a
David García <wlogz@hotmail.com>
2021-10-18 00:14:51 -0500

    Crea el archivo de login

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,4 @@
-export const API_URL = "http://localhost:1337"
+// export const API_URL = "http://localhost:1337" //Local 
+export const API_URL = "https://ecommerce2dstore-mintic.herokuapp.com" //Heroku
 export const TOKEN = "token"
 export const PRODUCTS = "products"
